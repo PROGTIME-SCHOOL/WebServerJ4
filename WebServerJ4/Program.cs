7a4e3e7076e3887e0535c6c014a40b84895003e4
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2022-11-06 11:06:16 +0300

    First Commit

---
+++ WebServerJ4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebServerJ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
